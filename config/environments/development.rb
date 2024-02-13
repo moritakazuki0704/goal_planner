437ef6396add7da51f2f40d6476bbd7a23f9cee1
EC2 Default User <ec2-user@ip-172-31-13-194.ap-northeast-1.compute.internal>
2024-02-13 16:49:49 +0000

    [add] 復元 gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "3d53cb401ae749808635609ab8064eee.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
