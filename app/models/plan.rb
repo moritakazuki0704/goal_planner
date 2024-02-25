b3c2a95d771e255b22c26a3070ce944dd0834906
EC2 Default User <ec2-user@ip-172-31-13-194.ap-northeast-1.compute.internal>
2024-02-25 07:34:25 +0000

    [fix] 進捗度ステータスのenumの名前を変更 名前の修正もれ

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,7 +1,7 @@
 class Plan < ApplicationRecord
   belongs_to :schedule
 
-  enum priority_status: { emergency: 0, important: 1, mission: 2, eliminate: 3 }
+  enum priority_status: { emergency: 0, important: 1, interruption: 2, eliminate: 3 }
   enum progress_status: { unstarted: 0, finish: 1, process: 2, postpone: 3, withdraw: 4 }
 
   validates :challenge, presence: true
@@ -15,7 +15,7 @@ class Plan < ApplicationRecord
   # ToDoリストにしたステータスが重要なことのタスクを表示
   scope :important, -> {where(programme: "true", priority_status: 1)}
   # ToDoリストにしたステータスがやるべきことのタスクを表示
-  scope :mission, -> {where(programme: "true", priority_status: 2)}
+  scope :interruption, -> {where(programme: "true", priority_status: 2)}
   # ToDoリストにしたステータスが辞めることのタスクを表示
   scope :eliminate, -> {where(programme: "true", priority_status: 3)}
 
